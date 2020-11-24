1adb68bc3adfe8df48f068dc8238d176626f5acc
ISWAD <iswad.brand@gmail.com>
2020-11-23 23:26:31 -0500

    Post

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def index():
-	return "Hello!!!"
+	data = request.get_json()
+	api_vec = data["ApiVec"]
+	result = api_vec
+	return jsonify(result = f"{result}")
 
 if __name__ == "__main__":
 	app.run()
